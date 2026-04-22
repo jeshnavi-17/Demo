59aa601fed965c8b16fa51cafb1e265277e1dc4d
jeshnavi-17 <sreejeshnavi179@gmail.com>
2026-04-22 16:07:10 +0530

    CORS access

--- src/main/java/com/example/demo/Config/WebConfig.java
+++ src/main/java/com/example/demo/Config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://<jeshnavi-17.github.io"
+                                "https://jeshnavi-17.github.io"
                         ) // React dev
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
